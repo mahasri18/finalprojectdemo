be4141e6bf033e29ad3a0df09f4c3eb9b58ec49c
mahasri18 <mahasri18shanmugaraj@gmail.com>
2023-07-27 15:17:54 +0530

    27th july

---
+++ CustomerServiceProvider/src/main/java/org/verizon/CustomerServiceProvider/CustomerServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package org.verizon.CustomerServiceProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceProviderApplication.class, args);
+	}
+
+}
